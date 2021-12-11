ea45afc104158cae435003a349ee33b3ce076e75
dev-everaldo-cyrino <alonedev3d2a@gmail.com>
2021-12-11 16:22:24 -0300

    fim

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = input('digite um numero: ')
-for x in range(1,len(n)):
-    x *=-1
-    print(n[x])
+n = input('numero: ')
+num = ''
+for x in range(1,len(n)+1):
+  x *=-1
+  num += num.join(n[x])
+else:
+  print(num)
